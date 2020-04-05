4006a854ecc4c015937a7ffd02e807d05c7c7238
culticreative <bri@culticreative.com>
2020-04-05 15:04:23 -0700

    Update class-cc-acf-field-places-biz.php

--- fields/class-cc-acf-field-places-biz.php
+++ fields/class-cc-acf-field-places-biz.php
@@ -94,7 +94,7 @@ class cc_acf_field_places_biz extends acf_field {
             </div>
             <div class="title" style="border:none">
                 <input
-                        data-maps-autocomplete
+                        data-maps-places_biz
                         class="search"
                         type="text"
                         placeholder="<?php _e( "Search for business...", 'cc' ); ?>"
